93e31afa11d8635712b1c6bd274b8f7c0badfb67
Sean Anderson <sanderson@foreground.co>
2024-02-02 19:38:42 +0000

    test

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use bevy::prelude::*;
 use bevy_ecs_tilemap::{TilemapPlugin, TilemapBundle};
 
 fn main() {
+
+
     App::new()
         .add_plugins((DefaultPlugins, TilemapPlugin))
         .add_state::<ExampleState>()
         .add_systems(Startup, (spawn_camera, spawn_tilemap_example))
-        .add_systems(OnExit(ExampleState::State1), despawn_tilemap_example)
-        .add_systems(OnEnter(ExampleState::State2), spawn_sprite_example)
+        // .add_systems(OnExit(ExampleState::State1), despawn_tilemap_example)
+        // .add_systems(OnEnter(ExampleState::State2), spawn_sprite_example)
+        .add_systems(PreUpdate, despawn_tilemap_example.run_if(on_exit_state1))
+        .add_systems(Update, spawn_sprite_example.run_if(on_enter_state2))
         .add_systems(Update, change_example_state)
         .run();
 }
@@ -54,4 +58,18 @@ pub fn change_example_state(state: Res<State<ExampleState>>, mut next_state: Res
         info!("changing state");
         next_state.set(ExampleState::State2);
     }
+}
+
+pub fn on_exit_state1(
+    state: Res<State<ExampleState>>,
+    next_state: Res<NextState<ExampleState>>,
+) -> bool {
+    state.get() == &ExampleState::State1 && next_state.0 == Some(ExampleState::State2)
+}
+
+pub fn on_enter_state2(
+    state: Res<State<ExampleState>>,
+    next_state: Res<NextState<ExampleState>>,
+) -> bool {
+    state.get() == &ExampleState::State1 && next_state.0 == Some(ExampleState::State2)
 }
