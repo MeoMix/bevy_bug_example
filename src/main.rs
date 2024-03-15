934918fd29138b031deeb174b6fd58b1294cec81
Sean Anderson <sanderson@foreground.co>
2024-03-15 01:50:02 +0000

    simplify MRE

--- src/main.rs
+++ src/main.rs
@@ -8,56 +8,19 @@ use bevy_ecs_tilemap::prelude::*;
 fn main() {
     App::new()
     .add_plugins((DefaultPlugins, TilemapPlugin))
-    .init_state::<AppState>()
-    .add_systems(Startup, setup)
-    .add_systems(
-        Update,
-        mark_loaded.run_if(in_state(AppState::NotReady)),
-    ).add_systems(
-        OnEnter(AppState::Ready),
-        (spawn_tilemap, spawn_sprite).chain()
-    )
+    .add_systems(Startup, (spawn_camera, spawn_tilemap).chain())
     .add_plugins(EguiPlugin)
     .run();
 }
 
-#[derive(States, Default, Hash, Clone, Copy, Eq, PartialEq, Debug)]
-pub enum AppState {
-    #[default]
-    NotReady,
-    Ready,
-}
-
-fn setup(mut commands: Commands) {
+fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-pub fn mark_loaded(mut next_app_state: ResMut<NextState<AppState>>) {
-    next_app_state.set(AppState::Ready);
-}
-
 pub fn spawn_tilemap(mut commands: Commands) {
-    let grid_size = TilemapGridSize { x: 1.0, y: 1.0 };
-    let map_type = TilemapType::default();
-    let map_size = TilemapSize { x: 144, y: 144 };
-
-    commands.spawn((
-        TilemapBundle {
-            grid_size,
-            map_type,
-            size: map_size,
-            storage: TileStorage::empty(map_size),
-            tile_size: TilemapTileSize { x: 128.0, y: 128.0 },
-            ..default()
-        },
-    ));
-}
-
-fn spawn_sprite(
-    mut commands: Commands,
-    mut tilemap_query: Query<(Entity, &mut TileStorage)>,
-) {
-    let (tilemap_entity, mut tile_storage) = tilemap_query.single_mut();
+    let tilemap_entity = commands.spawn_empty().id();
+    let map_size =  TilemapSize { x: 144, y: 144 };
+    let mut tile_storage = TileStorage::empty(map_size);
 
     let tile_pos = TilePos { x: 0, y: 0 };
 
@@ -69,4 +32,14 @@ fn spawn_sprite(
     }).id();
 
     tile_storage.set(&tile_pos, entity);
+
+    commands.entity(tilemap_entity).insert(
+        TilemapBundle {
+            grid_size: TilemapGridSize { x: 1.0, y: 1.0 },
+            size: map_size,
+            storage: tile_storage,
+            tile_size: TilemapTileSize { x: 128.0, y: 128.0 },
+            ..default()
+        },
+    );
 }
