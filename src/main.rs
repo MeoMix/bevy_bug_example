d8fee410889329e5b8dec88f3d71e11236649b54
Sean Anderson <sanderson@foreground.co>
2024-03-15 01:31:08 +0000

    MRE!

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,72 @@
+// This code should draw a white square in the center of the window if it runs properly.
+// One in every 5-10 runs no white square will render.
+// If the `multithreaded` feature is enabled in bevy's Cargo.toml then the square "always" renders (if it's a timing issue it becomes sufficiently difficult to surface with multithreading)
 use bevy::prelude::*;
-use bevy_ecs_tilemap::{TilemapPlugin, TilemapBundle};
+use bevy_egui::EguiPlugin;
+use bevy_ecs_tilemap::prelude::*;
 
 fn main() {
-
-
     App::new()
-        .add_plugins((DefaultPlugins, TilemapPlugin))
-        .add_state::<ExampleState>()
-        .add_systems(Startup, (spawn_camera, spawn_tilemap_example))
-        // .add_systems(OnExit(ExampleState::State1), despawn_tilemap_example)
-        // .add_systems(OnEnter(ExampleState::State2), spawn_sprite_example)
-        .add_systems(PreUpdate, despawn_tilemap_example.run_if(on_exit_state1))
-        .add_systems(Update, spawn_sprite_example.run_if(on_enter_state2))
-        .add_systems(Update, change_example_state)
-        .run();
+    .add_plugins((DefaultPlugins, TilemapPlugin))
+    .init_state::<AppState>()
+    .add_systems(Startup, setup)
+    .add_systems(
+        Update,
+        mark_loaded.run_if(in_state(AppState::NotReady)),
+    ).add_systems(
+        OnEnter(AppState::Ready),
+        (spawn_tilemap, spawn_sprite).chain()
+    )
+    .add_plugins(EguiPlugin)
+    .run();
 }
 
 #[derive(States, Default, Hash, Clone, Copy, Eq, PartialEq, Debug)]
-pub enum ExampleState {
+pub enum AppState {
     #[default]
-    State1,
-    State2,
+    NotReady,
+    Ready,
 }
 
-#[derive(Component)]
-pub struct ExampleTilemap;
-
-pub fn spawn_camera(mut commands: Commands) {
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-pub fn spawn_tilemap_example(mut commands: Commands) {
-    info!("spawn_tilemap_example");
+pub fn mark_loaded(mut next_app_state: ResMut<NextState<AppState>>) {
+    next_app_state.set(AppState::Ready);
+}
+
+pub fn spawn_tilemap(mut commands: Commands) {
+    let grid_size = TilemapGridSize { x: 1.0, y: 1.0 };
+    let map_type = TilemapType::default();
+    let map_size = TilemapSize { x: 144, y: 144 };
+
     commands.spawn((
-        ExampleTilemap,
         TilemapBundle {
+            grid_size,
+            map_type,
+            size: map_size,
+            storage: TileStorage::empty(map_size),
+            tile_size: TilemapTileSize { x: 128.0, y: 128.0 },
             ..default()
         },
     ));
 }
 
-pub fn despawn_tilemap_example(query: Query<Entity, With<ExampleTilemap>>, mut commands: Commands) {
-    info!("despawn_tilemap_example");
-    commands.entity(query.single()).despawn();
-}
+fn spawn_sprite(
+    mut commands: Commands,
+    mut tilemap_query: Query<(Entity, &mut TileStorage)>,
+) {
+    let (tilemap_entity, mut tile_storage) = tilemap_query.single_mut();
 
-// TODO: Determine why this panics after despawning `TilemapBundle` components
-pub fn spawn_sprite_example(mut commands: Commands) {
-    info!("spawn_sprite_example");
-    commands.spawn(SpriteBundle {
+    let tile_pos = TilePos { x: 0, y: 0 };
+
+    let entity = commands.spawn(TileBundle {
+        position: tile_pos,
+        tilemap_id: TilemapId(tilemap_entity),
+        texture_index: TileTextureIndex(0),
         ..default()
-    });
-}
+    }).id();
 
-pub fn change_example_state(state: Res<State<ExampleState>>, mut next_state: ResMut<NextState<ExampleState>>) {
-    if state.get() == &ExampleState::State1 {
-        info!("changing state");
-        next_state.set(ExampleState::State2);
-    }
-}
-
-pub fn on_exit_state1(
-    state: Res<State<ExampleState>>,
-    next_state: Res<NextState<ExampleState>>,
-) -> bool {
-    state.get() == &ExampleState::State1 && next_state.0 == Some(ExampleState::State2)
-}
-
-pub fn on_enter_state2(
-    state: Res<State<ExampleState>>,
-    next_state: Res<NextState<ExampleState>>,
-) -> bool {
-    state.get() == &ExampleState::State1 && next_state.0 == Some(ExampleState::State2)
+    tile_storage.set(&tile_pos, entity);
 }
